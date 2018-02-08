5a0e41eab92813bffc39b8ba1af5229ef7ac5f8b
CherishClark <cherish.g.clark@gmail.com>
2018-02-07 18:38:05 -0700

    creating a component with jsx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // Create a new component, and this component should produce some html
 
+const App = function () {
+  return <div> hi! </div>;
+}
 // Take this component's generated HTML and put it on the page (in the DOM)
